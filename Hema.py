50f791da2ff63d084f6046283ab9b554b7f3fb17
HEMA M <113386743+HEMA1188@users.noreply.github.com>
2023-07-20 13:39:43 +0530

    Add files via upload

---
+++ Hema.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+def sqrt(n):
+    a=n*n
+    return a
+
+   
+    
+
+
+# In[10]:
+
+
+sqrt(5)
+
+
+# In[15]:
+
+
+def inner(a,n):
+    if a//n==n:
+        return True
+    else:
+        return False
+
+
+# In[16]:
+
+
+inner(25,5)
+
+
+# In[ ]:
+
+
+
+
